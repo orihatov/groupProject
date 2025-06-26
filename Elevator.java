c5f8a55abf65dd7c440aa626c293bebc0e6b0363
razco1212 <raztektok@gmail.com>
2025-06-26 20:42:04 +0300

    Add height management to Elevator class with bounds checking

--- Elevator.java
+++ Elevator.java
@@ -1,16 +1,35 @@
 public class Elevator {
-    public boolean isAtTop() { return false; }
-    public boolean isAtBottom() { return false; }
+    
+    private double whatHeight;
+;
+
+    public double getHeight() {
+        return whatHeight;
+    }
+
+    public boolean isAtTop() { 
+        return whatHeight >= 100;
+    }
+    public boolean isAtBottom() {
+         return whatHeight <= 0;
+    }
     public void moveUp() {
         System.out.println("Moving up");
+       
     }
     public void moveDown() {
         System.out.println("Moving down");
     }
-    public void stop() {
-        System.out.println("Stopping");
+    public void setHeight(double height) {
+        if (height < 0 || height > 100) {
+            System.out.println("Height must be between 0 and 100");
+        }
+        else{
+            System.out.println("Setting height to " + height);
+        }
     }
-    
+
+
 
 
 
